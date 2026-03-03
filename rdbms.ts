b1d05507413221f042d36dd6d95ea773aedfd16d
Snu Seam <siamahme766@gmail.com>
2026-03-04 01:04:16 +0600

    ckdh

--- rdbms.ts
+++ rdbms.ts
@@ -3,3 +3,11 @@
 //     isActive true,
 //     name text
 //     `)
+
+
+// CREATE DATABASE testdb;
+// CREATE TABLE users (
+//     id SERIAL PRIMARY KEY,
+//     name VARCHAR(50),
+//     email VARCHAR(100)
+// );
